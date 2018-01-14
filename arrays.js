70ef0a792cb55b2c2a5dab629eecaab499e515f3
Xochitl Menjivar <xandra.menji@gmail.com>
2018-01-14 00:38:09 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -10,5 +10,6 @@ function addElementToBeginningOfArray(array, element){
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element){
-  return array.unshift(element);
+  array.unshift(element);
+  return array;
 }
